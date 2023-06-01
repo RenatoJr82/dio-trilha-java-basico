6df407aee040cda0bf1f1c5535c535604811824c
RenatoJr82 <renato.roberto.ajunior@gmail.com>
2023-06-01 09:13:42 -0300

    Conhecendo operadores do java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,50 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /* String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao); */
+
+        //Incremantação
+        /* int numero = 5;
+        numero = numero +1;
+        
+        System.out.println(numero);
+        System.out.println( ++ numero); */
+
+        /* //Ternário
+        int a, b;
+        a = 6;
+        b = 7;
+
+        //xString resultado = a==b ? "verdadeiro" : "falso"; //expressão boolean com string ou numeros
+        String resultado = "";
+
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado); */
+
+
+        
+    }
+}
