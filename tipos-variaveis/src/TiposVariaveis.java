37e6a473dceb8de94a41d3ce967b277baa7c5ff3
RenatoJr82 <renato.roberto.ajunior@gmail.com>
2023-06-01 09:15:06 -0300

    .

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        
+       /* final double SALARIO_MINIMO = 2.500; //CONSTANTES (final), NOME DA VARIAVEL SEMPRE EM CAIXA ALTA
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal; */
+
+       String nomeC = "linguagem " + "java";
+       
+       System.out.println(nomeC);
+
     }
 }
